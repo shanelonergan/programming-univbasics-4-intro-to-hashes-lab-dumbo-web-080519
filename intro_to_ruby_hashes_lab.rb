9766f8c3d57e3ed986548a5c336c2d9f7615ddd7
Shane Lonergan <sptlonergan@gmail.com>
2019-07-23 04:43:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,12 @@
 def new_hash
-  # return an empty hash
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    acoustic: "Martin"
+    electric: "Gibson"
+  }
 end
 
 def pioneer
